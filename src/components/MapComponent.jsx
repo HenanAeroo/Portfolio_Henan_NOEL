8453263321fbd0785646a11d2a9469249e1303e9
unknown <henan.noel@ynov.com>
2024-12-01 19:28:59 +0100

    Switch use effect to marker

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const MapComponent = () => {
   const [mapLoaded, setMapLoaded] = useState(false);
-  const apiKey = 'AIzaSyDtReNYj9XL_kHQZ0YVpIRmwrR6eZ_aANk';
+  const apiKey = "AIzaSyDtReNYj9XL_kHQZ0YVpIRmwrR6eZ_aANk";
 
   const containerStyle = {
-    width: '100%',
-    height: '400px',
+    width: "100%",
+    height: "400px",
   };
 
   // Coordonnées pour Rennes Ynov Campus
   const center = {
     lat: 48.1134,
-    lng: -1.6740,
+    lng: -1.674,
   };
 
   useEffect(() => {
@@ -22,14 +22,8 @@ const MapComponent = () => {
 
   return (
     <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={15}
-      >
-        {mapLoaded && (
-          <Marker position={center} />
-        )}
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+        <Marker position={center} />
       </GoogleMap>
     </LoadScript>
   );
